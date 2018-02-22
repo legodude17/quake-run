02e08a52d6c44325a942ce0ac99e463301ab7201
JDB <legodude17@users.noreply.github.com>
2018-02-22 07:55:20 -0800

    Add eslint

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const Subject = require('rxjs/Subject').Subject;
-const cp = require("child_process");
-const libs = require("./streamlibs");
+const { Subject } = require('rxjs/Subject');
+const cp = require('child_process');
+const libs = require('./streamlibs');
 
 module.exports = function run(cmd, delay) {
-  if (delay == null) delay=500;
-  func = function run() {
-    var subject = new Subject();
-    var args = cmd.split(' ');
-    var exec = args.shift();
-    var proc = cp.spawn(exec, args);
-    var done = false;
-    var messaging1 = {
+  if (delay == null) delay = 500;
+  const func = function run() {
+    const subject = new Subject();
+    const args = cmd.split(' ');
+    const exec = args.shift();
+    const proc = cp.spawn(exec, args, { cwd: process.cwd() });
+    let done = false;
+    const messaging1 = {
       cb(code) {
         if (!done) {
           done = true;
           return;
         }
-        if (code == 0) {
+        if (code === 0) {
           subject.complete();
         } else {
           subject.error(code);
@@ -24,12 +24,16 @@ module.exports = function run(cmd, delay) {
         }
       }
     };
-    var messaging2 = {cb: messaging1.cb};
-    proc.stdout.pipe(libs.splitByNewlines()).pipe(libs.delay(delay, messaging1)).on('data', data => subject.next(data.toString()));
-    proc.stderr.pipe(libs.splitByNewlines()).pipe(libs.delay(delay, messaging2)).on('data', data => subject.next(data.toString()));
-    proc.on('close', () => setTimeout(() => {messaging1.done(), messaging2.done();}, 500));
+    const messaging2 = { cb: messaging1.cb };
+    proc.stdout
+      .pipe(libs.splitByNewlines()).pipe(libs.delay(delay, messaging1))
+      .on('data', data => subject.next(data.toString()));
+    proc.stderr
+      .pipe(libs.splitByNewlines()).pipe(libs.delay(delay, messaging2))
+      .on('data', data => subject.next(data.toString()));
+    proc.on('close', () => setTimeout(() => { messaging1.done(); messaging2.done(); }, 500));
     return subject;
   };
-  func.displayName = "Run " + cmd;
+  func.displayName = `Run ${cmd}`;
   return func;
-}
+};
